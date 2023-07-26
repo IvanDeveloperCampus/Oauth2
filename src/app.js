9c686973458dcd242179f72e1860d769a774e51c
IvanDeveloperCampus <ivancampus2023@gmail.com>
2023-07-26 09:24:35 -0500

    feat: :construction: Autenticacion con discord

--- src/app.js
+++ src/app.js
@@ -1,40 +1,64 @@
 // index.js
-import express from 'express';
-import session from 'express-session';
-import passportHelper from './helpers/passportHelper.js'
+import express from "express";
+import session from "express-session";
+import passportHelper from "./helpers/passportHelper.js";
 
 const app = express();
 
-app.use(session({
-    secret: 'CLAVE_SECRETA_ENV', 
-    resave: false, 
-    saveUninitialized: false 
-}))
+app.use(
+  session({
+    secret: "CLAVE_SECRETA_ENV",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
 
 app.use(passportHelper.initialize()); // Inicializa passport
 app.use(passportHelper.session()); // Permite que passport use "express-session" para almacenar la sesión del usuario
 
-//Endpoints de autenticacion
-app.get("/auth/google", passportHelper.authenticate('google', { scope: ['email'] }));
+//Endpoints de autenticacion con google
+app.get(
+  "/auth/google",
+  passportHelper.authenticate("google", { scope: ["email"] })
+);
 
-app.get("/auth/google/callback", passportHelper.authenticate('google', 
-{ 
-    failureRedirect: '/auth/google',
-    successRedirect: '/saludo'
-}));
+app.get(
+  "/auth/google/callback",
+  passportHelper.authenticate("google", {
+    failureRedirect: "/auth/google",
+    successRedirect: "/saludo",
+  })
+);
 
+
+
+//Endpoints de autenticacion con discord
+app.get(
+  "/auth/discord",
+  passportHelper.authenticate("discord", { scope: ["email"] })
+);
+
+app.get(
+  "/auth/discord/callback",
+  passportHelper.authenticate("discord", {
+    failureRedirect: "/auth/discord",
+    successRedirect: "/saludo",
+  })
+);
 
 app.get("/logout", (req, res) => {
-    req.logout({}, err => console.log(err));
-    res.redirect('/auth/google');
+  req.logout({}, (err) => console.log(err));
+  res.redirect("/auth/google");
 });
 
 //Middleware para comprobar si esta autenticado
 const checkAuthentication = (req, res, next) =>
-        req.isAuthenticated() ? next() : res.redirect("/auth/google");
+  req.isAuthenticated() ? next() : res.redirect("/auth/discord");
 
-app.get("/saludo", checkAuthentication, (req, res) => res.send("Bienvenido a tu cuenta"))
+app.get("/saludo", checkAuthentication, (req, res) =>
+  res.send("Bienvenido a tu cuenta")
+);
 
 app.listen(5002, () => {
-    console.log(`Example app listening on http://localhost:5002/auth/google`);
+  console.log(`Example app listening on http://localhost:5002/auth/discord`);
 });
--- src/helpers/passportHelper.js
+++ src/helpers/passportHelper.js
@@ -1,11 +1,12 @@
 // passportHelper.js
 import passport from "passport";
 import { Strategy as GoogleStrategy } from "passport-google-oauth20";
+import { Strategy as DiscordStrategy} from 'passport-discord'
 
 const authUser = (accessToken, refreshToken, profile, done) => {
-    //console.log("accessToken", accessToken); // AccesToken es el token que le permite a nuestra aplicación acceder a los datos del usuario
-    //console.log("refreshToken", refreshToken); // RefreshToken es el token que le permite a nuestra aplicación obtener un nuevo accessToken cuando el actual expire
-     //console.log("profile", profile); // Profile es el objeto que contiene los datos del usuario
+    console.log("accessToken", accessToken); // AccesToken es el token que le permite a nuestra aplicación acceder a los datos del usuario
+    console.log("refreshToken", refreshToken); // RefreshToken es el token que le permite a nuestra aplicación obtener un nuevo accessToken cuando el actual expire
+     console.log("profile", profile); // Profile es el objeto que contiene los datos del usuario
     done(null, profile);
   };
 
@@ -19,6 +20,18 @@ passport.use(
         authUser
     )
 );
+
+var scopes = ['identify', 'email', 'guilds', 'guilds.join'];
+
+passport.use(new DiscordStrategy({
+    clientID: '1133759376562331668',
+    clientSecret: 'TqpzpS3Hr5pvlMocfYGjyqDWC1GmDPPo',
+    callbackURL: 'http://127.0.0.1:5002/auth/discord/callback',
+    scope: scopes
+},authUser
+)
+);
+
 passport.serializeUser((user, done) => done(null, user));
 
 passport.deserializeUser((user, done) => done(null, user));
